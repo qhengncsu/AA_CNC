4e4bd820678da46e7b47ba3de48205433b7739aa
qhengncsu <qheng@ncsu.edu>
2023-02-15 15:28:25 -0500

    Update demo_path.m

--- demo_path.m
+++ demo_path.m
@@ -1,8 +1,8 @@
-X = randn(1000,2000);
-beta = [ones(100,1);zeros(1900,1)];
+X = randn(1000,1000);
+beta = [ones(100,1);zeros(900,1)];
 y = X*beta + randn(1000,1)*std(X*beta);
-groups = cell(200,1);
-for i=1:200
+groups = cell(100,1);
+for i=1:100
     groups{i} = ((i-1)*10+1):(i*10);
 end
 %y = randn(1000,1);
@@ -10,7 +10,7 @@ y = (y-mean(y))/std(y);
 X = normc(X);
 
 tic
-xhat_matrix1 = srls_GMC_path(y, X, type="single", acceleration="original", screen=false, gamma=0.8);
+xhat_matrix1 = srls_GMC_path(y, X, type="single", acceleration="aa2", screen=false, gamma=0.8);
 toc
 tic
 xhat_matrix2 = srls_GMC_path(y, X, type="single", acceleration="aa2", screen=true, gamma=0.8);
@@ -18,11 +18,11 @@ toc
 norm(xhat_matrix1-xhat_matrix2)/norm(xhat_matrix2)
 
 tic
-xhat_matrix1 = srls_GMC_path(y, X, type="grouped", screen=false, lambda_min_ratio=0.1,groups=groups);
+xhat_matrix1 = srls_GMC_path(y, X, type="grouped", screen=false, lambda_min_ratio=0.01,groups=groups);
 toc
 tic
 %acceleration can also be nesterov
-xhat_matrix2 = srls_GMC_path(y, X, type="grouped",screen=true, lambda_min_ratio=0.1 ,groups=groups);
+xhat_matrix2 = srls_GMC_path(y, X, type="grouped",screen=true, lambda_min_ratio=0.01 ,groups=groups);
 toc
 norm(xhat_matrix1-xhat_matrix2)/norm(xhat_matrix2)
 
