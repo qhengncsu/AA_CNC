034cf2079fbce9a5bafdb286f05850b49b89d007
qhengncsu <qheng@ncsu.edu>
2023-03-11 18:13:06 -0500

    Update demo_acc.m

--- demo_acc.m
+++ demo_acc.m
@@ -11,16 +11,16 @@ end
 
 %%
 tic
-[xhat1, vhat1, res_norm_hist1] = srls_GMC_acc(y, X, 0.4, 'type', 'single', 'acceleration', 'original',"splitting","FB",'gamma',0.8);
+[xhat1, vhat1, res_norm_hist1] = srls_GMC_acc(y, X, 0.1, 'type', 'single', 'acceleration', 'original',"splitting","FB",'gamma',0.8);
 toc
 tic 
-[xhat2, vhat2, res_norm_hist2] = srls_GMC_acc(y, X, 0.4, 'type', 'single', 'acceleration', "original","splitting","FBF",'gamma',0.8);
+[xhat2, vhat2, res_norm_hist2] = srls_GMC_acc(y, X, 0.1, 'type', 'single', 'acceleration', "original","splitting","FBF",'gamma',0.8);
 toc
 tic 
-[xhat3, vhat3, res_norm_hist3] = srls_GMC_acc(y, X, 0.4, 'type', 'single', 'acceleration', "aa2","splitting","FB",'gamma',0.8);
+[xhat3, vhat3, res_norm_hist3] = srls_GMC_acc(y, X, 0.1, 'type', 'single', 'acceleration', "aa2","splitting","FB",'gamma',0.8);
 toc
 tic 
-[xhat4, vhat4, res_norm_hist4] = srls_GMC_acc(y, X, 0.4, 'type', 'single', 'acceleration', "aa2","splitting","FBF",'gamma',0.8);
+[xhat4, vhat4, res_norm_hist4] = srls_GMC_acc(y, X, 0.1, 'type', 'single', 'acceleration', "aa2","splitting","FBF",'gamma',0.8);
 toc
 figure;
 plot(log(res_norm_hist1), 'c-', 'LineWidth', 1.5)
